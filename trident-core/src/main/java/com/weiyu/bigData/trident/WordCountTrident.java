a2027d79ecb1235dc8a62892a8eaad609c8ad1ae
weiyu <weiyu@gomeholdings.com>
2018-05-04 10:24:23 +0800

    trident简单实例

--- trident-core/src/main/java/com/weiyu/bigData/trident/WordCountTrident.java
+++ trident-core/src/main/java/com/weiyu/bigData/trident/WordCountTrident.java
@@ -1,8 +1,19 @@
 package com.weiyu.bigData.trident;
 
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.LocalDRPC;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.generated.StormTopology;
 import org.apache.storm.trident.TridentState;
 import org.apache.storm.trident.TridentTopology;
 import org.apache.storm.trident.operation.builtin.Count;
+import org.apache.storm.trident.operation.builtin.FilterNull;
+import org.apache.storm.trident.operation.builtin.MapGet;
+import org.apache.storm.trident.operation.builtin.Sum;
 import org.apache.storm.trident.testing.FixedBatchSpout;
 import org.apache.storm.trident.testing.MemoryMapState;
 import org.apache.storm.trident.testing.Split;
@@ -11,7 +22,25 @@ import org.apache.storm.tuple.Values;
 
 public class WordCountTrident {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException, InterruptedException {
+        Config config = new Config();
+        config.setMaxSpoutPending(20);
+
+        if (args.length == 0) {
+            LocalDRPC drpc = new LocalDRPC();
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("wordCounter", config, buildTopology(drpc));
+            for (int i = 0; i < 100; i++) {
+                System.out.println("DRPC RESULT: " + drpc.execute("words", "cat the dog jumped"));
+                Thread.sleep(1000);
+            }
+        }
+        else {
+            StormSubmitter.submitTopology(args[0], config, buildTopology(null));
+        }
+    }
+
+    public static StormTopology buildTopology(LocalDRPC drpc){
         FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3, new Values("the cow jumped over the moon"),
                 new Values("the man went to the store and bought some candy"),
                 new Values("four score and seven years ago"),
@@ -21,6 +50,13 @@ public class WordCountTrident {
         TridentTopology topology = new TridentTopology();
         TridentState wordCounts = topology.newStream("spout1",spout).each(new Fields("sentence"),new Split(),new Fields("word"))
                 .groupBy(new Fields("word")).persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields(("count")))
-                .parallelismHint(6);
+                .parallelismHint(2);
+
+
+        topology.newDRPCStream("words", drpc).each(new Fields("args"), new Split(), new Fields("word")).groupBy(new Fields(
+                "word")).stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count")).each(new Fields("count"),
+                new FilterNull()).aggregate(new Fields("count"), new Sum(), new Fields("sum"));
+
+        return topology.build();
     }
 }
