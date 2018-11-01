6d66c7a3f0254fb9ffad492521d42663f23351d9
weiyu <weiyu005@ke.com>
2018-11-01 20:20:09 +0800

    storm+kafka

--- src/main/java/willem/weiyu/bigData/core/stormKafka/StormKafkaTopology.java
+++ src/main/java/willem/weiyu/bigData/core/stormKafka/StormKafkaTopology.java
@@ -16,8 +16,12 @@ public class StormKafkaTopology {
 
     public static void main(String[] args) {
         TopologyBuilder builder = new TopologyBuilder();
+        KafkaSpoutConfig conf = KafkaSpoutConfig.builder("10.26.27.81:9092","test")
+                .setProp("group.id","weiyu")
+                .setProcessingGuarantee(KafkaSpoutConfig.ProcessingGuarantee.AT_MOST_ONCE)//设置为AT_MOST_ONCE可以提交offset
+                .build();
 
-        builder.setSpout("kafka-spout",new KafkaSpout<>(KafkaSpoutConfig.builder("10.26.27.81:9092","test").build()));
+        builder.setSpout("kafka-spout",new KafkaSpout<>(conf));
         builder.setBolt("printBolt",new PrintBolt()).shuffleGrouping("kafka-spout");
 
         Config config = new Config();
